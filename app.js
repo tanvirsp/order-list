6977a1c687a381a6becd7381e962c1dee9a6fd80
Mir Raisul Tanvir <tanvirgraphic@gmail.com>
2022-06-26 02:56:42 +0600

    Delete Button is working

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const formData  = {};
+
+    [...this.elements].forEach(el=>{
+    
+        if(el.type != 'submit'){
+            formData[el.name] = el.value;
+        }
+    })
+
+    //showing data in HTML
+    showList(formData);
+    this.reset();
+    
+
+})
+
+
+function showList(obj){
+    const ul = document.getElementById('list');
+    const li = document.createElement('li');
+
+    const {order, price, client} = obj;
+
+    li.innerHTML = `<h3>${order}</h3>
+                    <span>${price}</span>
+                    <span>${client}</span>
+                    <div>
+                        <button class="edit-btn" onclick="editList(event)">Edit</button>
+                        <button class="delete-btn" onclick="deleteList(event)"> Delete</button>
+                    </div>
+                    `
+    ul.appendChild(li);
+}
+
+
+function deleteList(event){
+    event.target.parentNode.remove();
+}
+
+function editList(event){
+    console.log(event.target.parentNode.parentNode);
+    
+
+}
